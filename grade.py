59bee151e68db6987db34173689445a0c58c044f
fahadcip <fahadcip@gmail.com>
2023-12-23 08:51:03 +0600

    ok

--- grade.py
+++ grade.py
@@ -24,8 +24,8 @@ The University Grants Commission of Bangladesh recommended Grading system.
 https://ugc.portal.gov.bd/sites/default/files/files/ugc.portal.gov.bd/page/ad5e35a2_65ec_4a35_948a_b4a7a54de7ba/Uniform%20Recommended%20Grading%20System%20%281%29.pdf
 """
 
-def mymath(xkey, num):
-    s_num = num
+def mymath(xkey, xvalue):
+    s_num = xvalue
     if(s_num>=80):
         print(xkey , ": A+")
     elif(s_num<=79 and s_num>=75):
@@ -49,8 +49,8 @@ def mymath(xkey, num):
 
 
 for a, b in sub_dict.items():
-    mymath(xkey=a, num=b)
+    mymath(xkey=a, xvalue=b)
 
 #print(len(sub_dict))
 avg=(Bangla_sub+English_sub+Math_sub+Physics_sub+Biology_sub)/len(sub_dict)
-mymath(xkey="\nFinal Grade is", num=avg)
+mymath(xkey="\nFinal Grade is", xvalue=avg)
